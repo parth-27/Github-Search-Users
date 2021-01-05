4fff3b8cdd93505f16cfff8422abab23977b934b
parth-27 <parth1020patel@gmail.com>
2021-01-05 11:34:54 +0530

    initialize

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+
+// for routing to diff pages.
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact> {/*if exact prop is not present then Dashboard will be present in all the routes */}
+          <Dashboard></Dashboard>
+        </Route>
+
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/context/context.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+// context
+const GithubContext = React.createContext();
+
+// Provider, Consumer - GithubContext.Provider or GithubContext.Consumer
+
+
+// wrapping the whole application in githubprovider.
+// whatever is passed through value is accessible throughout the application.
+const GithubProvider = ({ children }) => {
+    return <GithubContext.Provider value={ "hello" }>{ children }</GithubContext.Provider>
+}
