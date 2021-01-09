cd15b1656fa2092a80eb4db5f89b6d05813eee30
parth-27 <parth1020patel@gmail.com>
2021-01-09 11:58:40 +0530

    doughnut

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,44 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
-};
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
 
-export default Doughnut2d;
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import CandyTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, CandyTheme);
+
+
+const ChartComponent = ({ data }) => {
+	const chartConfigs = {
+		type: "doughnut2d", // The chart type
+		width: "100%", // Width of the chart
+		height: "400", // Height of the chart
+		dataFormat: "json", // Data type
+		dataSource: {
+			// Chart Configuration
+			chart: {
+        caption: "Starts Per Language",
+        theme:"candy",
+        decimals: 0,
+        doughnutRadius: "40%",
+        showPercentValues:0,
+			},
+			// Chart Data
+			data, // data:data
+		}
+	};
+	return (<ReactFC {...chartConfigs} />);
+}
+
+
+export default ChartComponent;
