3c70b79b711c22e1620de5704a3ddd64070c4bd5
parth-27 <parth1020patel@gmail.com>
2021-01-10 06:02:33 +0530

    user repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,32 +2,148 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+	const { repos } = React.useContext(GithubContext);
+	
+	let languages = repos.reduce((total, item) => {
+		const { language,stargazers_count } = item;	// destructure from the item(repos)
+
+		if (!language)
+			return total;
+
+		if (!total[language])
+			total[language] = {
+				label: language,
+				value: 1,
+				stars:stargazers_count,
+			};
+		else
+			total[language] = {
+				...total[language],
+				value: total[language].value + 1,
+				stars: total[language].stars + stargazers_count,
+			};	// overwriting the on the spread operator.
+		
+		return total;
+	}, {});
+	
+	// converting to the array from the object.
+	const mostUsed = Object
+		.values(languages)
+		.sort((a, b) => {
+			// descending order
+			return b.value - a.value;
+		})
+		.slice(0,5);	// returning only first 5 popular languages.
+
+	
+	// most stars per language.
+
+	const mostPopular = Object
+		.values(languages)
+		.sort((a, b) => {
+			// sorting based on the most stars.
+			return b.stars - a.stars;
+		})
+		.map((item) => {
+			// assigning stars to the value because doughnut is looking for value.
+			return { ...item, value: item.stars };
+		})
+		.slice(0, 5);
+	
+	
+	// stars and forks
+	let { stars, forks } = repos.reduce((total,item) => {
+		const { stargazers_count, name, forks } = item;
+		
+		total.stars[stargazers_count] = { label: name, value: stargazers_count };
+		total.forks[forks] = { label: name, value: forks };
+
+		return total;
+	}, {
+			stars: {},
+			forks: {}
+	});
+
+	stars = Object
+		.values(stars)
+		.slice(-5)	// accessing only last 5 values
+		.reverse();	// in ascending order	
+	
+	forks = Object
+		.values(forks)
+		.slice(-5)	// accessing only last 5 values
+		.reverse();	// in ascending order	
+	
+	// const chartData = [
+	// 	{
+	// 		label: "Venezuela",
+	// 		value: "290"
+	// 	},
+	// 	{
+	// 		label: "Saudi",
+	// 		value: "260"
+	// 	},
+	// 	{
+	// 		label: "Canada",
+	// 		value: "180"
+	// 	},
+	// 	{
+	// 		label: "Iran",
+	// 		value: "140"
+	// 	},
+	// 	{
+	// 		label: "Russia",
+	// 		value: "115"
+	// 	},
+	// 	{
+	// 		label: "UAE",
+	// 		value: "100"
+	// 	},
+	// 	{
+	// 		label: "US",
+	// 		value: "30"
+	// 	},
+	// 	{
+	// 		label: "China",
+	// 		value: "30"
+	// 	}
+	// ];
+
+	return <section className="section">
+		<Wrapper className="section-center">
+			<Pie3D data={mostUsed} />
+			<Column3D data={stars}/>
+			<Doughnut2D data={mostPopular} />
+			<Bar3D data={forks}/>
+			{/* <ExampleChart data={ chartData }/> */}
+		</Wrapper>
+	</section>;
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  justify-items: center;
-  gap: 2rem;
-  @media (min-width: 800px) {
-    grid-template-columns: 1fr 1fr;
-  }
+	display: grid;
+  	justify-items: center;
+	gap: 2rem;
+	@media (min-width: 800px) {
+		grid-template-columns: 1fr 1fr;
+	}
 
-  @media (min-width: 1200px) {
-    grid-template-columns: 2fr 3fr;
-  }
+	@media (min-width: 1200px) {
+		grid-template-columns: 2fr 3fr;
+	}
 
-  div {
-    width: 100% !important;
-  }
-  .fusioncharts-container {
-    width: 100% !important;
-  }
-  svg {
-    width: 100% !important;
-    border-radius: var(--radius) !important;
-  }
+	div {
+		width: 100% !important;
+	}
+	.fusioncharts-container {
+		width: 100% !important;
+	}
+	svg {
+		width: 100% !important;
+		border-radius: var(--radius) !important;
+	}
 `;
 
 export default Repos;
