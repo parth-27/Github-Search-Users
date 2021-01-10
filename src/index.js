4ee35935856576d78104e3fab298f22fa43836b0
parth-27 <parth1020patel@gmail.com>
2021-01-10 06:00:44 +0530

    index

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    {/* wrapping whole application in githubprovider */}
-    <GithubProvider>
-      <App />
-    </GithubProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		{/* wrapping whole application in githubprovider */}
+		<Auth0Provider
+			domain={process.env.REACT_APP_DOMAIN}
+        	clientId={process.env.REACT_APP_CLIENT_ID}
+			redirectUri={window.location.origin}
+			cacheLocation='localstorage'
+		>			
+			<GithubProvider>
+				<App />
+			</GithubProvider>
+		</Auth0Provider>
+
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
